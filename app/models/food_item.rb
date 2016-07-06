bffbc91a8f5a1a81e18128b4120cf92b2b1fd302
Tanbui <hoangtan223@gmail.com>
2016-07-07 01:58:39 +0700

    Add validate for FoodItem and relayout its view

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -1,2 +1,12 @@
 class FoodItem < ApplicationRecord
+	validates :name, :section, :price, presence: true
+	validates :price, numericality: {greater_than_or_equal_to: 0 }
+
+	def image_url
+		if remote_image_url.present?
+			remote_image_url
+		else
+			"http://loremflickr.com/320/240/#{CGI.escape name}"
+		end
+	end
 end
